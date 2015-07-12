dac7147a299b933133aef1f81d838331ff426647
changjie <changjie@changjiedeMBP.lan>
2015-07-12 11:49:11 +0800

    Initial commit

---
+++ 2-3.py
@@ -0,0 +1,8 @@
+__author__ = 'changjie'
+
+import math
+
+def add(x,y,f):
+    return f(x)+f(y)
+
+print add(25,9,math.sqrt)
---
+++ 2-4.py
@@ -0,0 +1,6 @@
+__author__ = 'changjie'
+
+def format_name(s):
+    return s[0].upper()+s[1:].lower()
+
+print map(format_name,['adam','LISA','barT'])
